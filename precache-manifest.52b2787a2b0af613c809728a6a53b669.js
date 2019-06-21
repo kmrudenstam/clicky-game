c7c5da610be2ee94a6a9940886c5f48c8c923f30
Karin Rudenstam <Karin@Karins-MacBook-Pro.local>
2019-06-20 18:34:52 -0700

    Updates

---
+++ precache-manifest.52b2787a2b0af613c809728a6a53b669.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a9f9d163f64f8e5f9a3562dd3cb4ff8",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "97bcfcf394d1d7ebda30",
+    "url": "/clicky-game/static/css/main.15043026.chunk.css"
+  },
+  {
+    "revision": "9508b3e097f5d4db1edd",
+    "url": "/clicky-game/static/js/2.97807068.chunk.js"
+  },
+  {
+    "revision": "97bcfcf394d1d7ebda30",
+    "url": "/clicky-game/static/js/main.c750d7a7.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
